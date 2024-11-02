2e17eb26afaee5731cb4c60692c136625ba2d00d
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-11-02 13:06:09 +0100

    Update CanLogServerPlugin.js

--- CanLogServerPlugin.js
+++ CanLogServerPlugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'CanLogServer Plugin',
-    version: '1.0',
+    version: '2.0',
     author: 'Highpoint',
     frontEndPath: 'CanLogServer/canlog.js'
 }
